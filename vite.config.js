a1575a0ae989e0e5fb8b4ac0498178af34ffe56d
Isabela Silva <albuquerqueisabela951@gmail.com>
2025-03-07 15:08:40 -0300

    comit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.quotable.io',
+        target: 'https://api.quotable.io/quotes/random',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
